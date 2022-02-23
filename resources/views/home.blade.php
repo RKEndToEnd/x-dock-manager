0c74f5f9a9ef560831b3d07fd9ba3d5f2f10e195
RK EndToEnd <rkendtoend@gmail.com>
2022-02-23 23:48:18 +0100

    after login page, auth group in routes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,19 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row justify-content-center" style="margin: 30px;">
+        <div class="col-md-auto">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header text-center">{{ config('app.name') }}</div>
 
-                <div class="card-body">
+                <div class="card-body text-center">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                        Jesteś zalogowany jako: {{ Auth::user()->name }} {{Auth::user()->surname}}, e-mail:{{Auth::user()->email}}
                 </div>
             </div>
         </div>

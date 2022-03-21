71ab256ca4ed81fec8dce2cbaa07f74bae36cfbe
RK EndToEnd <rkendtoend@gmail.com>
2022-03-21 12:29:22 +0100

    add user form, regoistration form - validation messeges display correct

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,79 +1,90 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-header text-center">Rejestracja nowego użytkownika</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header text-center">Rejestracja nowego użytkownika</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="input-group mb-3">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" autocomplete="name" autofocus placeholder="Imię">
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="input-group mb-3">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
+                                       name="name" value="{{ old('name') }}" autocomplete="name" autofocus
+                                       placeholder="Imię">
 
                                 @error('name')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                        </div>
-
-                        <div class="input-group mb-3">
-                                <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" autocomplete="surname" autofocus placeholder="Nazwisko">
+                            </div>
+                            <div class="input-group mb-3">
+                                <input id="surname" type="text"
+                                       class="form-control @error('surname') is-invalid @enderror" name="surname"
+                                       value="{{ old('surname') }}" autocomplete="surname" autofocus
+                                       placeholder="Nazwisko">
 
                                 @error('surname')
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                        </div>
-
-                        <div class="input-group mb-3">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" placeholder="Email">
+                            </div>
+                            <div class="input-group mb-3">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                       name="email" value="{{ old('email') }}" autocomplete="email" placeholder="Email">
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                        </div>
-
-                        <div class="input-group mb-3">
-                            <select id="depot_id" class="form-control" name="depot_id">
-                                <option value="null">Depot - wybierz z listy</option>
+                            </div>
+                            <div class="input-group mb-3">
+                                <select id="depot_id" class="form-control @error('depot_id') is-invalid @enderror"
+                                        name="depot_id">
+                                    <option value="">Depot - wybierz z listy</option>
                                     @foreach($depots as $depot)
-                                        <option id="depot_id" name="depot_id" value="{{ $depot->id }}">{{ $depot->name }}</option>
+                                        <option id="depot_id" name="depot_id"
+                                                value="{{ $depot->id }}">{{ $depot->name }}</option>
                                     @endforeach
-                            </select>
-                        </div>
-
-                        <div class="input-group mb-3">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password" placeholder="Haslo">
+                                </select>
+                                @error('depot_id')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="input-group mb-3">
+                                <input id="password" type="password"
+                                       class="form-control @error('password') is-invalid @enderror" name="password"
+                                       autocomplete="new-password" placeholder="Haslo">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                        </div>
-
-                        <div class="input-group mb-3">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password" placeholder="Powtórz hasło">
-                        </div>
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-outline-primary">
-                                    Zarejestruj
-                                </button>
                             </div>
-                        </div>
-                    </form>
+                            <div class="input-group mb-3">
+                                <input id="password-confirm" type="password" class="form-control"
+                                       name="password_confirmation" autocomplete="new-password"
+                                       placeholder="Powtórz hasło">
+                            </div>
+                            <div class="row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-outline-primary">
+                                        Zarejestruj
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,22 +6,22 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header"><h4>Użytkownicy</h4></div>
-                        <div class="card-body">
-                            <table class=" table table-hover table-bordered table-striped" id="users-all">
-                                <thead>
-                                    <th>#</th>
-                                    <th>Imię</th>
-                                    <th>Nazwisko</th>
-                                    <th>ID</th>
-                                    <th>Email</th>
-                                    <th>Depot</th>
-                                    <th>Akcje</th>
-                                </thead>
-                                <tbody></tbody>
-                            </table>
-                        </div>
+                    <div class="card-body">
+                        <table class=" table table-hover table-bordered table-striped" id="users-all">
+                            <thead>
+                            <th>#</th>
+                            <th>Imię</th>
+                            <th>Nazwisko</th>
+                            <th>ID</th>
+                            <th>Email</th>
+                            <th>Depot</th>
+                            <th>Akcje</th>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
                     </div>
                 </div>
+            </div>
             @hasrole('super-admin|admin')
             <div class="col-md-4">
                 <div class="card">
@@ -29,62 +29,42 @@
                     <div class="card-body">
                         <form method="POST" action="{{ route('add.user') }}" id="create-user">
                             @csrf
-                            <div class="input-group mb-3">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" autocomplete="name" autofocus placeholder="Imię">
-
-                                @error('name')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            <div class="form-group mb-3">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}"
+                                       autocomplete="name" autofocus placeholder="Imię">
+                                <span class="text-danger error-text name_error"></span>
                             </div>
-
-                            <div class="input-group mb-3">
-                                <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" autocomplete="surname" autofocus placeholder="Nazwisko">
-
-                                @error('surname')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            <div class="form-group mb-3">
+                                <input id="surname" type="text" class="form-control" name="surname"
+                                       value="{{ old('surname') }}" autocomplete="surname" autofocus
+                                       placeholder="Nazwisko">
+                                <span class="text-danger error-text surname_error"></span>
                             </div>
-
-                            <div class="input-group mb-3">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" placeholder="Email">
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            <div class="form-group mb-3">
+                                <input id="email" type="email" class="form-control" name="email"
+                                       value="{{ old('email') }}" autocomplete="email" placeholder="Email">
+                                <span class="text-danger error-text email_error"></span>
                             </div>
-
-                            <div class="input-group mb-3">
+                            <div class="form-group mb-3">
                                 <select id="depot_id" class="form-control" name="depot_id">
-                                    <option value="null">Depot - wybierz z listy</option>
+                                    <option value="">Depot - wybierz z listy</option>
                                     @foreach($depots as $depot)
-                                        <option id="depot_id" name="depot_id" value="{{ $depot->id }}">{{ $depot->name }}</option>
+                                        <option id="depot_id" name="depot_id"
+                                                value="{{ $depot->id }}">{{ $depot->name }}</option>
                                     @endforeach
                                 </select>
-                                @error('depot_id')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <span class="text-danger error-text depot_id_error"></span>
                             </div>
-
-                            <div class="input-group mb-3">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password" placeholder="Haslo">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            <div class="form-group mb-3">
+                                <input id="password" type="password" class="form-control" name="password"
+                                       autocomplete="new-password" placeholder="Haslo">
+                                <span class="text-danger error-text password_error"></span>
                             </div>
-
-                            <div class="input-group mb-3">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password" placeholder="Powtórz hasło">
+                            <div class="form-group mb-3">
+                                <input id="password-confirm" type="password" class="form-control"
+                                       name="password_confirmation" autocomplete="new-password"
+                                       placeholder="Powtórz hasło">
+                                <span class="text-danger error-text password_confirmation_error"></span>
                             </div>
                             <div class="row mb-0">
                                 <div class="col-md-6 offset-md-4">
@@ -110,36 +90,36 @@
     const deleteUserUrl = "{{ route('delete.user') }}";
 
     $.ajaxSetup({
-        headers:{
-            'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content')
-        }
+    headers:{
+    'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content')
+    }
     });
 
     //Add new user - testing, will be replaced with modal
     $('#add-user-form').on('submit', function (e){
-        e.preventDefault()
-        var form = this;
-        $.ajax({
-            url:$(form).attr('action'),
-            method:$(form).attr('method'),
-            data:new FormData(form),
-            processData:false,
-            dataType:'json',
-            contentType:false,
-            beforeSend:function (){
-                $(form).find('span.error-text').text('')
-            },
-            success:function (data){
-                if(data.code == 0){
-                    $.each(data.error, function (prefix, val){
-                        $(form).find('span.'+prefix+'_error').text(val[0]);
-                    });
-                    }else{
-                        $(form)[0].reset();
-                        alert(data.msg);
-                    }
-            }
-        });
+    e.preventDefault()
+    var form = this;
+    $.ajax({
+    url:$(form).attr('action'),
+    method:$(form).attr('method'),
+    data:new FormData(form),
+    processData:false,
+    dataType:'json',
+    contentType:false,
+    beforeSend:function (){
+    $(form).find('span.error-text').text('')
+    },
+    success:function (data){
+    if(data.code == 0){
+    $.each(data.error, function (prefix, val){
+    $(form).find('span.'+prefix+'_error').text(val[0]);
+    });
+    }else{
+    $(form)[0].reset();
+    alert(data.msg);
+    }
+    }
+    });
     });
 
 @endsection
